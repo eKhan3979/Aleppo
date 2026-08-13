5469cbe6f75e74e04327a5c40b3da364becfae5a
eKhan3979 <edson3979@gmail.com>
2026-08-13 11:34:32 -0300

    Versão 43

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+import express from 'express';
+
+const app = express();
+
+const API_TOKEN = process.env.API_TOKEN;
+
+function authenticate(req, res, next) {
+    const token = req.headers.authorization?.replace("Bearer ", "");
+
+    if (!token || token != API_TOKEN) {
+        return res.status(401).json({
+            error: "Token inválido ou ausente"            
+        });
+    }
+
+    next();
+}
+
+app.get('/api/hello', authenticate, (req, res) => {
+    res.json({
+        message: "API funcionando !"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`API rodando na porta ${PORT}`);
+});
+
+
+/*
 const app = require('./app');
 
 const PORT = 3000;
@@ -5,3 +37,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT}`);
 });
+*/
