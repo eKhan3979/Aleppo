b9b84e28914a0bffc1010634eefd436d66a564d9
eKhan3979 <edson3979@gmail.com>
2026-08-26 15:56:17 -0300

    Versão 86

--- dados/bolao/empresaDados.js
+++ dados/bolao/empresaDados.js
@@ -4,6 +4,26 @@ class empresaDados {
 
     constructor() {};
 
+    async get(idEmpresa) {
+        let conn;
+
+        try {
+            let sql = "Call u258112148_1.SpBEmpresa_Get(" + idEmpresa + ");";
+
+            conn = await conexao.getConnection();
+
+            const rows = await conn.query(
+                sql
+            );
+
+            return rows[0];
+        } catch (erro) {
+            throw erro;
+        } finally {
+            if (conn) await conn.release();
+        }
+    }
+
     async lista() {
         let conn;
 
